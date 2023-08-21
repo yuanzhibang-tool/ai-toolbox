f1a9667ae8203a282a84d50eb470fa8390254ddb
shandawei <shandawei@me.com>
2023-08-21 21:42:12 +0000

    improve project info

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,9 @@
 # !/bin/bash
+cp README.md code/README.md
 cd code
 flit install
 version=`dev-version --section=patch --toml_path=pyproject.toml`
 dev-release --version=$version
 flit build
+rm code/README.md
 flit publish    # This will ask for username and password
